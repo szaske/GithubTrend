4130c0b2e83b5721ad21efd3e7bf4340be4ecb54
szaske <github@zaske.com>
2018-07-17 15:49:04 -0700

    Implementing the get Bookmarked Projects use case

---
+++ Domain/src/main/java/com/loc8r/domain/interactors/GetBookmarkedProjects.kt
@@ -0,0 +1,28 @@
+/**
+ * In Clean Architecture, the Domain layer defines Use Cases
+ * which define operations which can be performed in our
+ * application. In this lesson, we’ll be implementing the Get
+ * Bookmarked Projects Use Cases class for our application so
+ * that we can retrieve a list of bookmarked projects within
+ * our external data source through our domain layer.
+ */
+
+package com.loc8r.domain.interactors
+
+import com.loc8r.domain.interfaces.PostExecutionThread
+import com.loc8r.domain.interactors.ObservableUseCaseBase
+import com.loc8r.domain.models.Project
+import com.loc8r.domain.interfaces.ProjectsRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class GetBookmarkedProjects @Inject constructor(
+        private val projectsRepository: ProjectsRepository,
+        postExecutionThread: PostExecutionThread)
+    : ObservableUseCaseBase<List<Project>, Nothing>(postExecutionThread){
+
+    public override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectsRepository.getBookmarkedProjects()
+    }
+
+}
