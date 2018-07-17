fc1c773215eb8e3b99b6211c58db1eadbc53c0d4
szaske <github@zaske.com>
2018-07-17 14:52:11 -0700

    Setting up the business data for the Domain layer

---
+++ Domain/src/main/java/com/loc8r/domain/model/Project.kt
@@ -0,0 +1,14 @@
+package com.loc8r.domain.model
+
+class Project(
+        val id: String,
+        val name: String,
+        val fullName: String,
+        val starCount: String,
+        val dateCreated: String,
+        val ownerName: String,
+        val ownerAvatar: String,
+        val isBookMarked: Boolean
+) {
+
+}
