9d86dcc793fc6b74a76510d726f0f5745d74686a
szaske <github@zaske.com>
2018-07-19 09:00:49 -0700

    Creating the data store factory class

--- Data/src/main/java/com/loc8r/data/ProjectsDataStoreFactory.kt
+++ Data/src/main/java/com/loc8r/data/ProjectsDataStoreFactory.kt
@@ -1,4 +1,28 @@
+/**
+ * This class helps us choose between 2 passed data stores
+ */
 package com.loc8r.data
 
-class ProjectsDataStoreFactory {
+import com.loc8r.data.interfaces.ProjectsDataStore
+import javax.inject.Inject
+
+class ProjectsDataStoreFactory @Inject constructor(
+        private val projectsCacheDataStore: ProjectsCacheDataStore,
+        private val projectsRemoteDataStore: ProjectsRemoteDataStore) {
+
+    /**
+     * Returns the proper data store depending on the status of the cache
+     */
+    open fun getDataStore(projectsCached: Boolean,
+                          cacheExpired: Boolean): ProjectsDataStore {
+        return if (projectsCached && !cacheExpired) {
+            projectsCacheDataStore
+        } else {
+            projectsRemoteDataStore
+        }
+    }
+
+    open fun getCachedDataStore(): ProjectsDataStore {
+        return projectsCacheDataStore
+    }
 }
