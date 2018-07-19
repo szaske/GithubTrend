b222da53b33e8926b5e00f8c412e69c74162fbc2
szaske <github@zaske.com>
2018-07-19 08:47:11 -0700

    Creating the data layer remote data store

---
+++ Data/src/main/java/com/loc8r/data/ProjectsDataStoreFactory.kt
@@ -0,0 +1,4 @@
+package com.loc8r.data
+
+class ProjectsDataStoreFactory {
+}
---
+++ Data/src/main/java/com/loc8r/data/ProjectsRemoteDataStore.kt
@@ -0,0 +1,43 @@
+/**
+ * If you look at the ProjectsRemote interface you'll see that it only implements
+ * one method getProjects().  So for the other methods I'll just throw an exception
+ * to show that something has been implemented incorrectly if someone attempt to
+ * call these methods
+ */
+
+package com.loc8r.data
+
+import com.loc8r.data.interfaces.ProjectsDataStore
+import com.loc8r.data.interfaces.ProjectsRemote
+import com.loc8r.data.models.ProjectEntity
+import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class ProjectsRemoteDataStore @Inject constructor(
+        private val projectsRemote: ProjectsRemote
+): ProjectsDataStore {
+    override fun getProjects(): Observable<List<ProjectEntity>> {
+        return projectsRemote.getProjects()
+    }
+
+    override fun saveProjects(projects: List<ProjectEntity>): Completable {
+        throw UnsupportedOperationException("Saving projects is not supported by the remote data store")
+    }
+
+    override fun clearProjects(): Completable {
+        throw UnsupportedOperationException("Clearing projects is not supported by the remote data store")
+    }
+
+    override fun getBookmarkedProjects(): Observable<List<ProjectEntity>> {
+        throw UnsupportedOperationException("Getting bookmarked projects is not supported by the remote data store")
+    }
+
+    override fun setProjectAsBookmarked(projectId: String): Completable {
+        throw UnsupportedOperationException("Bookmarking a project is not supported by the remote data store")
+    }
+
+    override fun setProjectAsNotBookmarked(projectId: String): Completable {
+        throw UnsupportedOperationException("Unbookmarking a projects is not supported by the remote data store")
+    }
+}
