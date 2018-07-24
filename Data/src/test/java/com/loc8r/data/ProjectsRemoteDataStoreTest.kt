cb3a8498377899fdb253ef0c97977e54e1ab44e1
szaske <github@zaske.com>
2018-07-24 14:26:35 -0700

    Adding unit tests for ProjectsRemoteDataStore class

---
+++ Data/src/test/java/com/loc8r/data/ProjectsRemoteDataStoreTest.kt
@@ -0,0 +1,65 @@
+package com.loc8r.data
+
+import com.loc8r.data.interfaces.ProjectsRemote
+import com.loc8r.data.models.ProjectEntity
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
+import org.junit.Test
+import utils.DataFactory
+import utils.ProjectFactory
+
+class ProjectsRemoteDataStoreTest {
+    private val remote = mock<ProjectsRemote>()
+    private val store = ProjectsRemoteDataStore(remote)
+
+    // Stub Methods
+    //
+
+    private fun stubRemoteGetProjects(observable: Observable<List<ProjectEntity>>){
+        whenever(remote.getProjects())
+                .thenReturn(observable)
+    }
+
+    @Test
+    fun GetProjectsCompletes(){
+        stubRemoteGetProjects(Observable.just(listOf(ProjectFactory.makeProjectEntity())))
+        val testObserver = store.getProjects().test()
+        testObserver.assertComplete()
+    }
+
+    @Test
+    fun GetProjectsReturnsCorrectData(){
+        val response = listOf(ProjectFactory.makeProjectEntity())
+        stubRemoteGetProjects(Observable.just(response))
+        val testObserver = store.getProjects().test()
+        testObserver.assertValue(response)
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun saveProjectsThrowsException(){
+        store.saveProjects(listOf()).test()
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun clearProjectsThrowsException(){
+        store.clearProjects().test()
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun getBookmarkedProjectsThrowsException(){
+        store.getBookmarkedProjects().test()
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun setProjectAsBookmarkedThrowsException(){
+        store.setProjectAsBookmarked(DataFactory.randomString()).test()
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun setProjectAsNotBookmarkedThrowsException(){
+        store.setProjectAsNotBookmarked(DataFactory.randomString()).test()
+    }
+
+
+}
