27bfd9f41f34630020c0cf15b064356f287b5a52
szaske <github@zaske.com>
2018-08-05 10:51:26 -0700

    Creating the data models for the Presentation Module

---
+++ Presentation/src/main/java/com/loc8r/presentation/models/ProjectView.kt
@@ -0,0 +1,11 @@
+package com.loc8r.presentation.models
+
+class ProjectView(val id: String,
+                  val name: String,
+                  val fullName: String,
+                  val starCount: String,
+                  val dateCreated: String,
+                  val ownerName: String,
+                  val ownerAvatar: String,
+                  val isBookmarked: Boolean) {
+}
