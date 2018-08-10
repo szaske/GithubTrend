5f56d8cd804f425e306565cbe51481a8560a6bbc
szaske <github@zaske.com>
2018-08-09 23:15:37 -0700

    Creating the UI Project Model

---
+++ Mobile_ui/src/main/java/com/loc8r/mobile_ui/models/Project.kt
@@ -0,0 +1,11 @@
+package com.loc8r.mobile_ui.models
+
+class Project(val id: String,
+              val name: String,
+              val fullName: String,
+              val starCount: String,
+              val dateCreated: String,
+              val ownerName: String,
+              val ownerAvatar: String,
+              val isBookmarked: Boolean) {
+}
